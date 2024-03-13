21a699e1fc0f668c10ef5ea08bd3eff75917e803
sai_tejas <saitejassharma@gmail.com>
2024-03-13 21:59:57 +0530

    Add files via upload

---
+++ Recursive_Bubble_sort.java
@@ -0,0 +1,31 @@
+public class Recursive_Bubble_sort {
+    public static void main(String[] args) {
+        int arr[]={10,50,20,40,20};
+        int n=arr.length;
+        System.out.println("Before Swapping");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+
+        recursive_sort(arr,n);
+        System.out.println("After Swapping");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        
+
+    }
+    static void recursive_sort(int arr[],int n){
+        if(n==1) return;
+        for(int j=0;j<=n-2;j++){
+            if(arr[j]>arr[j+1]){ 
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        recursive_sort(arr, n-1);
+    }
+}
